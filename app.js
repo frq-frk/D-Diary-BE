0825ca6a9dc38b1d6609711b3e66d42c5f5c1d82
Farooq <mullafarooq4@gmail.com>
2023-04-12 09:44:13 +0530

    preparing for the staging server deployment

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 require('dotenv').config()
 require('./src/utils/db')
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 const express = require('express')
 const cors = require('cors')
 
+var RateLimit = require("express-rate-limit");
+
 const app = express()
+
+app.use(compression()); //compressor that compressos responses to improve performance
+app.use(helmet()); //library to protect app from well know vulnerablities
+
+// Set up rate limiter: maximum of twenty requests per minute
+var limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
 
 const authMiddleware = require('./src/services/firebase/auth-middleware')
 const routes = require('./src/v1/routes/index')
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
